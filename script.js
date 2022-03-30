9383224ca956331b22d5c360e5a59be54942ea85
Gian-P <pimentel.giancarlo1@gmail.com>
2022-03-29 20:40:44 -0400

    Finished sidebar and header

---
+++ script.js
@@ -0,0 +1,11 @@
+let windowWidth = window.innerWidth;
+let windowHeight = window.innerHeight;
+
+let screenWidth = screen.width;
+let screenHeight = screen.height;
+
+console.log(windowWidth);
+console.log(windowHeight);
+
+console.log(screenWidth);
+console.log(screenHeight);
